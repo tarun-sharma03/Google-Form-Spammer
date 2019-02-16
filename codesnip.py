6c051e36ec5eaf6dd47b918803c4cf18e9853e7c
pulkitgupta2k <42977200+pulkitgupta2k@users.noreply.github.com>
2019-02-17 00:54:34 +0530

    commit_2
    Runs on python3
    fixed faker

--- codesnip.py
+++ codesnip.py
@@ -3,6 +3,7 @@ import http.client
 from faker import Faker
 
 fake=Faker()
+fake_IN=Faker('hi_IN')
 
 def post(link,num,faker):
 	form_entry=['']
@@ -13,15 +14,35 @@ def post(link,num,faker):
 		choice='y'
 		while(choice=='y'):
 			print("Enter form data entry: ")
-			form_entry.append(raw_input())
+			form_entry.append(input())
 			print("Enter the value:	")
-			form_entry_data.append(raw_input())
+			form_entry_data.append(input())
 			print("Add more entries?(y/N)")
-			choice=raw_input()
+			choice=input()
 		for i in range(len(form_entry)):
 			if(i==0):
 				continue
 			payload+="\r\nContent-Disposition: form-data; name=\""+str(form_entry[i])+"\"\r\n\r\n"+str(form_entry_data[i])+"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW"
+		conn = http.client.HTTPSConnection("docs.google.com")
+		
+		payload+="--"
+
+		headers = {
+	    'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+	    'cache-control': "no-cache",
+	    'postman-token': "f7281180-f919-b589-709a-a77457e89d08"
+	    }
+		print("Sending")
+		for x in range (num):
+			conn.request("POST", "/forms/u/0/d/e/"+link+"/formResponse", payload, headers)
+			res = conn.getresponse()
+			data = res.read()
+			print('.')
+
+		if(len(data)>100):
+			print('Successful!')
+		else:
+			print('ERROR!!')
 		
 	
 
@@ -29,44 +50,55 @@ def post(link,num,faker):
 		choice='y'
 		while(choice=='y'):
 			print("Enter form data entry: ")
-			form_entry.append(raw_input())
-			print("Choose the type of value:	\n(name,number,email,address,none)")
-			form_entry_data.append(faker_input(raw_input()))
+			form_entry.append(input())
+			print("Choose the type of value:	\n(name,number,email,word,sentence,or enter the value)")
+			form_entry_data.append(input())
 			print("Add more entries?(y/N)")
-			choice=raw_input()
-		for i in range(len(form_entry)):
-			if(i==0):
-				continue
-			payload+="\r\nContent-Disposition: form-data; name=\""+str(form_entry[i])+"\"\r\n\r\n"+str(form_entry_data[i])+"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW"
-	payload+="--"
-	conn = http.client.HTTPSConnection("docs.google.com")
+			choice=input()
+		
+		conn = http.client.HTTPSConnection("docs.google.com")
 
-	headers = {
-    'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-    'cache-control': "no-cache",
-    'postman-token': "f7281180-f919-b589-709a-a77457e89d08"
-    }
-	print("Sending")
-	for x in range (num):
-		conn.request("POST", "/forms/u/0/d/e/"+link+"/formResponse", payload, headers)
-		res = conn.getresponse()
-		data = res.read()
-		print('.')
+		headers = {
+	    'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+	    'cache-control': "no-cache",
+	    'postman-token': "f7281180-f919-b589-709a-a77457e89d08"
+	    }
+		print("Sending")
+		for x in range (num):
+			payload_ls="------WebKitFormBoundary7MA4YWxkTrZu0gW"
+			for i in range(len(form_entry)):
+				if(i==0):
+					continue
+				payload_ls+="\r\nContent-Disposition: form-data; name=\""+str(form_entry[i])+"\"\r\n\r\n"+str(faker_input(form_entry_data[i]))+"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW"
+			payload_ls+="--"
+			print(payload_ls)
+			conn.request("POST", "/forms/u/0/d/e/"+link+"/formResponse", payload_ls, headers)
+			res = conn.getresponse()
+			data = res.read()
+			print('.')
 
-	if(len(data)>100):
-		print('Successful!')
-	else:
-		print('ERROR!!')	
+		if(len(data)>100):
+			print('Successful!')
+		else:
+			print('ERROR!!')
+
+	
+		
 
 
 def faker_input(type):
 	if(type=="name"):
-		name=fake.name()
-		print(name)
 		return fake.name()
+	if(type=="number"):
+		return fake.msisdn()
+	if(type=="word"):
+		return fake.word()
+	if(type=="sentence"):
+		return fake.sentence()
+	if(type=="email"):
+		return fake.email()
 	else:
-		print("Enter the value:	")
-		return raw_input()
+		return type
 	
 
 argument=sys.argv
@@ -94,6 +126,3 @@ for i in range(len(argument)):
 
 if(help_flag==0):
 	post(link,num,faker)
-
-
-	
